671e2dcd1e50628bf84166ed12059d3cc5c69282
MuhammadYeasinArafath <mdyeasinarafathofficial@gmail.com>
2022-08-19 02:33:12 +0600

    first commit

---
+++ README.md
@@ -0,0 +1,21 @@
+# -Hello-World-in-java
+“Hello, World!” in java.
+<br>
+you can run this code in your turminal.
+<br>
+install any java code runner extension in vs code.
+<br>
+or,
+<br>
+how to run java code in your terminal:
+<br>
+   step 1: //Run this line to compile:
+   <br>
+javac programName.java [javac hello.java]
+   <br>
+  step 2: //Run this line to run:
+  <br>
+java programName [java hello]
+<br>
+
+
---
+++ hello.java
@@ -0,0 +1,17 @@
+public class hello{
+    public static void main(String args[]) {
+        System.out.println("Hello, World!");
+    }
+}
+//you can run this code in your turminal.
+//install any java code runner extension in vs code.
+
+
+
+/*how to run java code in your terminal:
+   //Run this line to compile
+javac programName.java [javac hello.java]
+
+//Run this line to run
+java programName [java hello]
+*/
